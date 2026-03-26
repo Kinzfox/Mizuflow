1985b24ccba2240b42569b957240d334e1beb87d
Kinzfox <m-9074007@moe-dl.edu.my>
2026-03-26 18:16:04 +0800

    Add files via upload

---
+++ pom/service-worker.js
@@ -0,0 +1,20 @@
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open("Minux").then(cache => {
+      return cache.addAll([
+        "./",
+        "./index.html",
+        "./style.css",
+        "./script.js",
+        "./manifest.json"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
+
